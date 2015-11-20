37102e6696c0a99a58f7dd9fed2793b050e7095f
Matt <mattmccoy94@gmail.com>
2015-11-19 20:30:34 -0500

    organized files and tweaked script.m

---
+++ part-1/qr_fact_househ.m
@@ -0,0 +1,29 @@
+function [Q,R,error]=qr_fact_househ(A)
+
+%A Cornell website was used to help make this function
+%as http://www.cs.cornell.edu/~bindel/class/cs6210-f09/lec18.pdf
+
+n=length(A);
+R=A; 
+Q=eye(n); 
+for k=1:n-1
+    x=zeros(n,1);
+    x(k:n,1)=R(k:n,k);
+    g=norm(x);
+    v=x; v(k)=x(k)+g;
+    s=norm(v);
+    if s~=0, w=v/s; u=2*R'*w;
+    R=R-w*u'; 
+    Q=Q-2*Q*(w*w'); 
+    end
+end
+C=zeros(n);
+for i=1:n	
+	for j=1:n	
+			C(i,j)=C(i,j) + Q(i,:) * R(:,j);   
+	end	
+end	
+errorMatrix = C - A;
+maxError = max(max(errorMatrix));
+minError = min(min(errorMatrix));
+error = max(abs(maxError),abs(minError));
--- script.m
+++ script.m
@@ -2,11 +2,17 @@
 % TODO: Get use input
 A = [1 1 1 1;1 2 3 4;1 3 6 10;1 4 10 20]
 B = [1;1/2;1/3;1/4]
-
+cd('./part-1');
+% disp('starting lu_fact.');
 [L,U,error] = lu_fact(A)
-
-A = [3 4;3 1]
-v = [1;0]
+cd('../')
+cd('./part-3')
+A = [3 4;3 1];
+v = [1;0];
 t = 1/5;
 s = 10;
 [e_value, e_vector] = power_method(A,v,t,s)
+
+
+% END OF SCRIPT
+cd('../');
