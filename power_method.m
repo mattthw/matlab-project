067858981beab951af7c16584e811f4c7ab6d956
Matt <mattmccoy94@gmail.com>
2015-11-19 20:10:57 -0500

    finished power method (fixed inner dimm issue)

--- power_method.m
+++ power_method.m
@@ -1,28 +1,41 @@
 % A: n × n matrix A with floating-point real numbers as entries
 % v: vector v of n floating-point real numbers that serves as
 %      the initial guess for an eigenvector of A
+%      !!NUMBER OF ROWS SHOULD == # OF COLUMNS IN A!!
+%       ex: A=[1 2 5;3 1 4] v=[1;4;6]
 % t: tolerance parameter ε (a positive floating-point real number)
 %      that determines when the approximation is close enough
 % s: positive integer N giving the maximum number of times to
 %      iterate the power method before quitting.
-function [e_value, e_vector] = power_method(A,v,t,s)
-e_value = 0;
-n = length(v);
-u_vector = v;
-prev_u = u_vector;
-prev_val = e_value;
-count = 0;
-% while( (((e_value - prev_val) > t) && ((u_vector - prev_u) > t) && (count < s)) || (prev_u==u_vector) )
-while(true)
-    count=count+1;
-    prev_u=u_vector
-    prev_val=e_value;
-
-    u_vector=A*prev_u.';
-    numerator=max(u_vector);
-    denominator=max(prev_u);
-    e_value=numerator/denominator
-end
+% e_value
+function [e_value, e_vector] = power_method(A,e_vector,t,s)
+    disp('Starting power_method');
+    e_value = 0;
+    % n = length(e_vector);
+    prev_u = e_vector;
+    prev_val = e_value;
+    count = 0;
+    while(true)
+        count=count+1;
+        % e_vector
+        prev_u=e_vector;
+        prev_val=e_value;
+        e_vector=A*prev_u;
+        numerator=max(e_vector);
+        denominator=max(prev_u);
+        e_value=numerator/denominator;
+        if (abs(e_value - prev_val) <= t || count > s)
+            if (e_value - prev_val <= t)
+                disp('Stopped at tolerance condition')
+            end
+            if (count > s)
+                disp('Stopped at max count')
+            end
+            % e_vector
+            % e_value
+            return;
+        end
+    end
 end
 
 
--- script.m
+++ script.m
@@ -6,7 +6,7 @@ B = [1;1/2;1/3;1/4]
 [L,U,error] = lu_fact(A)
 
 A = [3 4;3 1]
-v = [1 0]
+v = [1;0]
 t = 1/5;
 s = 10;
 [e_value, e_vector] = power_method(A,v,t,s)
