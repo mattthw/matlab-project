f3838bde90f5611a99b61cda98c3da13c40fe79b
Lee, Min Ho <mlee432@gatech.edu>
2015-11-21 16:34:44 -0500

    Update jacobi_iter.m

--- part-2/jacobi_iter.m
+++ part-2/jacobi_iter.m
@@ -1,6 +1,8 @@
-
-function [x, N] = jacobi_iter(A,b,x0, e, M)
-n = length(b);      % find n
+% Program 2.2  Jacobi Method
+% Inputs: full or sparse matrix a, r.h.s. b,
+%         number of Jacobi iterations k
+% Output: solution x
+function [x, N] = jacobi_iter(A,b,x0, e, M, N)
 len = length(A);
 D = zeros(len, len);
 d = [1;1;1];
@@ -8,13 +10,13 @@ x = x0;
 for l = 1 : len
     D(l,l) = A(l,l);
 end
-r = A - D;
+A = A - D;
 for m = 1 : M
   N = m;
-  x = (b - r * x) ./ d;
+  x = (b - A * x) ./ d;
   if (m > 1)
     app = abs(norm(x-xt));
-    if (e > app)
+    if (ge(e,app))
         return
     end
   end
