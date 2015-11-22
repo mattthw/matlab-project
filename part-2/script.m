ce80cf2a2b07de59766113a035f9f54282807aac
Lee, Min Ho <mlee432@gatech.edu>
2015-11-22 16:01:14 -0500

    Update script.m

--- part-2/script.m
+++ part-2/script.m
@@ -5,20 +5,41 @@ b = [.1;.1;.1];
 M = 100;
 e = .00005;
 xapx = [0;0;0];
-xext = [9/190;28/475;33/475]
+xext = [9/190;28/475;33/475];
+jacobi_N = 0;
 for j = 1 : M
     x0 = [-1 + 2.*rand(1,1);-1 + 2.*rand(1,1);-1 + 2.*rand(1,1);];
-    [x, N] = jacobi_iter(A, b, x0, e, M)
+    [x, N] = jacobi_iter(A, b, x0, e, M);
     xapx = xapx + x;
+    jacobi_N = jacobi_N + N;
 end
 xapx = xapx ./ M;
-disp('Xapprox =');
+disp('Jacobi x approximation =');
 disp(xapx);
-disp('Xexact =');
+disp('Jacobi x exact =');
 disp(xext);
-disp('Error of approximation =');
+disp('Error of approximation for Jacobi =');
 xerror = abs(norm(xapx - xext));
 disp(xerror)
+jacobi_N = jacobi_N / 100
+yapx = [0;0;0];
+yext = [9/190;28/475;33/475];
+gs_N = 0;
+for j = 1 : M
+    x0 = [-1 + 2.*rand(1,1);-1 + 2.*rand(1,1);-1 + 2.*rand(1,1);];
+    [y, N] = gs_iter(A, b, x0, e, M);
+    yapx = yapx + y;
+    gs_N = gs_N + N;
+end
+yapx = yapx ./ M;
+disp('Gauss-Siedel x approximation =');
+disp(yapx);
+disp('Gauss-Siedel x exact =');
+disp(yext);
+disp('Error of approximation for Gauss-Siedel =');
+yerror = abs(norm(yapx - yext));
+disp(yerror)
+gs_N = gs_N / 100
 
-% y = gs_iter(A,b,M)
+Ratio = jacobi_N / gs_N
 cd('../');
