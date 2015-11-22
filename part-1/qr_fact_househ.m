556dea4e00d5d5138802444f120288ba9fa9f27d
Ye, Jerry <jye48@gatech.edu>
2015-11-21 20:07:00 -0500

    Update qr_fact_househ.m

--- part-1/qr_fact_househ.m
+++ part-1/qr_fact_househ.m
@@ -1,26 +1,30 @@
 function [Q,R,error]=qr_fact_househ(A)
 
 %A Cornell website was used to help make this function
-%as http://www.cs.cornell.edu/~bindel/class/cs6210-f09/lec18.pdf
+%at https://www.cs.berkeley.edu/~wkahan/Math128/ReflRotn.pdf
 
 n=length(A);
 R=A; 
-Q=eye(n); 
-for k=1:n-1
+Q=zeros(n);
+for x=1:n
+    Q(x,x)=1;
+end
+for z=1:n-1
     x=zeros(n,1);
-    x(k:n,1)=R(k:n,k);
+    x(z:n,1)=R(z:n,z);
     g=norm(x);
-    v=x; v(k)=x(k)+g;
+    v=x; v(z)=x(z)+g;
     s=norm(v);
-    if s~=0, w=v/s; u=2*R'*w;
+    if s~=0, w=v/s; 
+    u=2*R'*w;
     R=R-w*u'; 
     Q=Q-2*Q*(w*w'); 
     end
 end
 C=zeros(n);
-for i=1:n	
-	for j=1:n	
-			C(i,j)=C(i,j) + Q(i,:) * R(:,j);   
+for x=1:n	
+	for y=1:n	
+    C(x,y)=C(x,y) + Q(x,:) * R(:,y);   
 	end	
 end	
 errorMatrix = C - A;
