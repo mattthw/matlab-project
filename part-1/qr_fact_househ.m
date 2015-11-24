ffed9eeb5ec0178b9a1868e9963a562a40dfdc9b
Ye, Jerry <jye48@gatech.edu>
2015-11-23 19:44:26 -0500

    Update qr_fact_househ.m

--- part-1/qr_fact_househ.m
+++ part-1/qr_fact_househ.m
@@ -7,25 +7,26 @@ n=length(A);
 R=A; 
 Q=zeros(n);
 for x=1:n
-    Q(x,x)=1;
+ Q(x,x)=1;
 end
 for z=1:n-1
-    x=zeros(n,1);
-    x(z:n,1)=R(z:n,z);
-    g=norm(x);
-    v=x; v(z)=x(z)+g;
-    s=norm(v);
-    if s~=0, w=v/s; 
-    u=2*R'*w;
-    R=R-w*u'; 
-    Q=Q-2*Q*(w*w'); 
-    end
+ x=zeros(n,1);
+ x(z:n,1)=R(z:n,z);
+ g=norm(x);
+ v=x; v(z)=x(z)+g;
+ s=norm(v);
+ if s~=0, w=v/s; 
+  u=2*R'*w;
+  R=R-w*u'; 
+  Q=Q-2*Q*(w*w'); 
+ end
 end
+
 C=zeros(n);
 for x=1:n	
-	for y=1:n	
-    C(x,y)=C(x,y) + Q(x,:) * R(:,y);   
-	end	
+ for y=1:n	
+  C(x,y)=C(x,y) + Q(x,:) * R(:,y);   
+ end	
 end	
 errorMatrix = C - A;
 maxError = max(max(errorMatrix));
